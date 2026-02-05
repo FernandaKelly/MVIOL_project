5c95c0f21715c8d900c1b1f21db7d5c65329f081
Fernanda Kelly <42969258+FernandaKelly@users.noreply.github.com>
2026-02-05 16:44:09 -0300

    Atualização.

--- leituraDados_LEI15610.R
+++ leituraDados_LEI15610.R
@@ -347,29 +347,17 @@ ggplot() +
  theme(axis.text.y = element_text(face = "bold"))
 
 
+##################################
 
+dados_classificados %>% 
+  janitor::tabyl(classificacao, sexo_vitima, show_na = FALSE) %>% 
+  janitor::adorn_percentages()
 
-dados_classificados %>% janitor::tabyl(classificacao, sexo_vitima, show_na = FALSE) %>% janitor::adorn_percentages()
+##################################
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-####################
+##################################
+# SEXO: FEMININO
+##################################
 
 dados_enquadramento_M <- dados2024_art3 %>% 
   dplyr::filter(sexo_vitima == "Feminino") %>% 
@@ -652,7 +640,268 @@ ggplot() +
 
 
 
-
+dados_enquadramento_M <- dados2024_art3 %>% 
+  dplyr::filter(sexo_vitima == "Feminino") %>% 
+  mutate(
+    classificacao = case_when(
+      
+      # ===============================
+      # 1. CRIMES CONTRA A MULHER
+      # ===============================
+      tipo_enquadramento %in% c(
+        "VIOLENCIA PSICOLOGICA CONTRA MULHER ART 147B",
+        "DESCUMPRIMENTO DE MEDIDA PROTETIVA DE URGENCIA",
+        "FEMINICIDIO ART 121 PAR 2 VI",
+        "IMPORTUNACAO SEXUAL",
+        "ESTUPRO",
+        "VIOLACAO SEXUAL MEDIANTE FRAUDE"
+      ) ~ "Crimes contra a mulher",
+      
+      # ===============================
+      # 2. CRIMES CONTRA A DIGNIDADE SEXUAL
+      # ===============================
+      tipo_enquadramento %in% c(
+        "ASSEDIO SEXUAL",
+        "EXPLORACAO SEXUAL INFANTO-JUVENIL",
+        "CASA DE PROSTITUICAO",
+        "CONDUTAS RELACIONADAS À PEDOFILIA NA INTERNET",
+        "RUFIANISMO",
+        "PORNOGRAFIA INFANTO-JUVENIL",
+        "SATISFACAO DE LASCIVIA MEDIANTE PRESENCA DE CRIANCA OU ADOLESCENTE"
+      ) ~ "Crimes contra a dignidade sexual",
+      
+      # ===============================
+      # 3. CRIMES CONTRA A PESSOA
+      # ===============================
+      tipo_enquadramento %in% c(
+        "AMEACA",
+        "LESAO CORPORAL",
+        "LESAO CORPORAL LEVE",
+        "LESAO CORPORAL GRAVE",
+        "LESAO CORPORAL GRAVISSIMA",
+        "LESAO CORPORAL SEGUIDA DE MORTE",
+        "LESAO CORPORAL CULPOSA",
+        "LESAO CORPORAL CULPOSA DIRECAO VEIC AUTOMOTOR",
+        "VIAS DE FATO",
+        "HOMICIDIO DOLOSO",
+        "HOMICIDIO CULPOSO",
+        "HOMICIDIO CULPOSO DIRECAO VEIC AUTOMOTOR",
+        "HOMICIDIO DECORRENTE DE OPOSICAO A INTERVENCAO POLICIAL",
+        "SEQUESTRO E CARCERE PRIVADO",
+        "SEQUESTRO RELAMPAGO",
+        "PERSEGUICAO",
+        "MAUS TRATOS",
+        "VIOLACAO DE DOMICILIO",
+        "VIOLACAO DE SEPULTURA",
+        "VILIPENDIO A CADAVER",
+        "RIXA",
+        "PERIGO A VIDA/SAUDE DE OUTREM",
+        "INCENDIO DOLOSO",
+        "INCENDIO CULPOSO",
+        "INFANTICIDIO",
+        "INDUZIMENTO INSTIGAO AUXILIO AO SUICIDIO OU A AUTOMULTILAO"
+      ) ~ "Crimes contra a pessoa",
+      
+      # ===============================
+      # 4. CRIMES CONTRA O PATRIMÔNIO
+      # ===============================
+      tipo_enquadramento %in% c(
+        "ESBULHO POSSESSORIO",
+        "FURTO SIMPLES",
+        "FURTO QUALIFICADO",
+        "FURTO DE VEICULO",
+        "FURTO EM VEICULO",
+        "FURTO DE TELEFONE CELULAR",
+        "FURTO DE DOCUMENTO",
+        "FURTO DE FIOS E CABOS",
+        "FURTO ABIGEATO",
+        "FURTO DE ARMA",
+        "FURTO DE CARGA EM CAMINHAO",
+        "FURTO DE COISA COMUM ART 156  SUBTRAIR O CONDOMINO COHERDEIRO OU SOCIO PARA SI OU PARA OUTREM A QUEM LEGITIMAMENTE A DETEM A COISA COMUM",
+        "FURTO DE DEFENSIVO AGRICOLA",
+        "FURTO QUALIFICADO EM VEICULO",
+        "FURTO SIMPLES EM RESIDENCIA",
+        "FURTO VEICULO AUTOMOTOR TRANSPORTE OUTRO ESTADO",
+        "FURTO/ARROMBAMENTO",
+        "FURTO/ARROMBAMENTO A CAIXA ELETRONICO",
+        "FURTO/ARROMBAMENTO A ESTABELECIMENTO DE ENSINO",
+        "FURTO/ARROMBAMENTO DE RESIDENCIA",
+        "FURTO/ARROMBAMENTO ESTABELECIMENTO BANCARIO",
+        "FURTO/ARROMBAMENTO ESTABELECIMENTO COMERCIAL",
+        "OUTROS FURTOS",
+        "ROUBO A PEDESTRE",
+        "ROUBO DE VEICULO",
+        "ROUBO DE TELEFONE CELULAR",
+        "ROUBO A RESIDENCIA",
+        "ROUBO COM LESOES",
+        "ROUBO COM MORTE",
+        "OUTROS ROUBOS",
+        "EXTORSAO",
+        "EXTORSAO MEDIANTE SEQUESTRO",
+        "ESTELIONATO",
+        "ESTELIONATO  FRAUDE ELETRONICA",
+        "ESTELIONATO  FRAUDE COM A UTILIZACAO DE ATIVOS VIRTUAIS VALORES MOBILIARIOS OU ATIVOS FINANCEIROS",
+        "DANO",
+        "RECEPTACAO",
+        "RECEPTACAO CULPOSA",
+        "RECEPTACAO QUALIFICADA",
+        "RECEPTACAO DE VEICULO",
+        "RECEPTACAO DE ANIMAL",
+        "OUTRAS FRAUDES",
+        "APROPRIACAO INDEBITA",
+        "FRAUDE A CREDORES",
+        "FRAUDE NO COMERCIO",
+        "FRAUDE NO PAGAMENTO POR MEIO DE CHEQUE"
+      ) ~ "Crimes contra o patrimônio",
+      
+      # ===============================
+      # 5. CRIMES CONTRA A HONRA
+      # ===============================
+      tipo_enquadramento %in% c(
+        "CALUNIA",
+        "DIFAMACAO",
+        "INJURIA",
+        "DENUNCIACAO CALUNIOSA"
+      ) ~ "Crimes contra a honra",
+      
+      # ===============================
+      # 6. CRIMES CONTRA A ADMINISTRAÇÃO PÚBLICA
+      # ===============================
+      tipo_enquadramento %in% c(
+        "CORRUPCAO ATIVA",
+        "CONCUSSAO",
+        "PECULATO",
+        "TRAFICO DE INFLUENCIA",
+        "FAVORECIMENTO REAL",
+        "FAVORECIMENTO PESSOAL",
+        "PREVARICACAO",
+        "EXERCICIO ARBITRARIO OU ABUSO DE PODER",
+        "EXERCICIO ARBITRARIO PROPRIAS RAZOES",
+        "USURPACAO DE FUNCAO PUBLICA",
+        "ORGANIZACAO CRIMINOSA ART 2 DA LEI 12850 DE 2013",
+        "VIOLACAO DE SIGILO FUNCIONAL",
+        "RESISTENCIA"
+      ) ~ "Crimes contra a administração pública",
+      
+      # ===============================
+      # 7. CRIMES CONTRA A FÉ PÚBLICA
+      # ===============================
+      tipo_enquadramento %in% c(
+        "FALSIDADE IDEOLOGICA",
+        "FALSIFICACAO DE DOCUMENTOS PARTICULARES",
+        "FALSIFICACAO DE PAPEIS/DOCUMENTOS PUBLICOS",
+        "FALSO TESTEMUNHO OU FALSA PERICIA",
+        "USO DE DOCUMENTO FALSO",
+        "FALSA IDENTIDADE",
+        "SIMULACAO QUALIDADE DE FUNCIONARIO",
+        "USO ILEGITIMO DE UNIFORME OU DISTINTIVO",
+        "EXTRAVIO SONEGACAO OU INUTILIZACAO DE LIVRO OU DOCUMENTO",
+        "SUBTRACAO OU INUTILIZACAO DE LIVRO OU DOCUMENTO",
+        "VIOLACAO DE CORRESPONDENCIA"
+      ) ~ "Crimes contra a fé pública",
+      
+      # ===============================
+      # 8. CRIMES RELACIONADOS A ARMAS
+      # ===============================
+      tipo_enquadramento %in% c(
+        "DISPARO DE ARMA DE FOGO",
+        "PORTE ILEGAL DE ARMA DE FOGO DE USO PERMITIDO",
+        "POSSE IRREGULAR DE ARMA DE FOGO DE USO PERMITIDO",
+        "POSSE OU PORTE ILEGAL DE ARMA DE FOGO DE USO PROIBIDO",
+        "POSSE OU PORTE ILEGAL DE ARMA DE FOGO DE USO RESTRITO",
+        "PORTE ARMA BRANCA",
+        "FABRICO COMERCIO OU DENTENCAO DE ARMA BRANCA OU MUNICAO",
+        "FORNECER ARMA, MUNICAO, EXPLOSIVO A CRIANCA/ADOLESCENTE",
+        "COMERCIO ILEGAL DE ARMA DE FOGO",
+        "TRAFICO INTERNACIONAL DE ARMA DE FOGO"
+      ) ~ "Crimes relacionados a armas",
+      
+      # ===============================
+      # 9. CRIMES DE TRÂNSITO
+      # ===============================
+      tipo_enquadramento %in% c(
+        "EMBRIAGUEZ ART 306",
+        "EMBRIAGUEZ - ART. 306",
+        "DIRECAO PERIGOSA (RACHA) ART 308",
+        "DIRIGIR SEM HABILITACAO GERANDO PERIGO DE DANO ART. 309",
+        "FUGA DE LOCAL DE ACIDENTE - ART. 305",
+        "VIOLAR A SUSPENSAO OU PROIBICAO DE DIRIGIR VEICULO ART.307",
+        "TRAFEGAR COM VELOCIDADE INCOMPATIVEL COM A SEGURANCA ART.311",
+        "INOVAR ARTIFICIOSAMENTE,EM CASO DE ACIDENTE TRANSITO ART.312",
+        "PERMITIR,CONFIAR,OU ENTREG. DIRECAO A PESSOA S/HABIL.ART 310"
+      ) ~ "Crimes de trânsito",
+      
+      # ===============================
+      # 10. CRIMES RELACIONADOS A DROGAS
+      # ===============================
+      tipo_enquadramento %in% c(
+        "ENTORPECENTES - TRAFICO",
+        "ENTORPECENTE  ASSOCIACAO",
+        "ENTORPECENTES  POSSE"
+      ) ~ "Crimes relacionados a drogas",
+      
+      # ===============================
+      # 11. CRIMES CONTRA CRIANÇAS E ADOLESCENTES
+      # ===============================
+      tipo_enquadramento %in% c(
+        "ABANDONO DE INCAPAZ",
+        "SUBTRACAO DE INCAPAZ",
+        "ESTUPRO DE VULNERAVEL",
+        "SERVIR BEBIDA ALCOOLICA A VULNERAVEIS",
+        "FORNECER PRODUTOS CRIANCA/ADOLESCENTE QUE CAUSAM DEPENDENCIA",
+        "OUTROS CRIMES CONTRA A CRIANCA/ADOLESCENTE"
+      ) ~ "Crimes contra crianças e adolescentes",
+      
+      # ===============================
+      # 12. CRIMES DE DISCRIMINAÇÃO
+      # ===============================
+      tipo_enquadramento %in% c(
+        "HOMOFOBIA TRANSFOBIA",
+        "PRECONCEITO RACA COR",
+        "PRECONCEITO DE RACA E COR",
+        "PRECONCEITO DEFICIENCIA",
+        "PRECONCEITO ETNIA PROCEDENCIA NACIONAL",
+        "PRECONCEITO RELIGIAO",
+        "INJURIA DISCRIMINATORIA",
+        "DISCRIMINACAO A PESSOA COM DEFICIENCIA",
+        "APOLOGIA AO NAZISMO",
+        "INJURIA QUALIFICADA  RACA COR ETNIA RELIGIAO OU ORIGEM",
+        "DISCRIMINACAO CONTRA O IDOSO",
+      ) ~ "Crimes de discriminação",
+      
+      # ===============================
+      # 13. CRIMES CONTRA GRUPOS VULNERÁVEIS
+      # ===============================
+      tipo_enquadramento %in% c(
+        "ABANDONO DE PESSOA COM DEFICIENCIA",
+        "OUTROS CRIMES CONTRA O IDOSO",
+        "ABANDONO DE IDOSO",
+        "OMISSAO DE SOCORRO CONTRA O IDOSO",
+        "MAUS TRATOS CONTRA O IDOSO",
+        "APROPRIACAO INDEBITA DE BEM DE IDOSO",
+        "RETENCAO DO CARTAO DE PESSOA COM DEFICIENCIA"
+      ) ~ "Crimes contra grupos vulneráveis",
+      
+      # ===============================
+      # 14. CRIMES AMBIENTAIS E CONTRA ANIMAIS
+      # ===============================
+      tipo_enquadramento %in% c(
+        "CRIME DE POLUICAO E OUTROS CRIMES AMBIENTAIS",
+        "CRIMES CONTRA A FAUNA",
+        "CRIMES CONTRA A FLORA",
+        "OUTROS CRIMES CONTRA MEIO AMBIENTE",
+        "CRUELDADE CONTRA ANIMAIS",
+        "OMISSAO DE CAUTELA NA GUARDA OU CONDUCAO DE ANIMAIS",
+        "SUPRESSAO OU ALTERACAO DE MARCA EM ANIMAIS",
+        "USURPACAO DE AGUAS"
+      ) ~ "Crimes ambientais e contra animais",
+      
+      # ===============================
+      # 15. OUTROS
+      # ===============================
+      TRUE ~ "Outros crimes"
+    )
+  )
 
 
 
